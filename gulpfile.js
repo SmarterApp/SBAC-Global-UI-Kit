79356a6887d4202367bd52e0d5954df5f74b7dd2
Aaron Sleeper <asleeper@fairwaytech.com>
2017-07-18 14:28:19 -0700

    removed the need for a row-end class and added style file for wkhtmltopdf

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ var gulp = require('gulp'),
     src = 'src',
     dist = 'dist',
     paths = {
-        styles: ['src/less/bundle.less'],
+        styles: [
+            'src/less/bundle.less',
+            'src/less/wkhtmltopdf.less'
+        ],
         fonts: [
             'node_modules/bootstrap/fonts/**/*',
             'node_modules/font-awesome/fonts/**/*'
@@ -14,18 +17,26 @@ var gulp = require('gulp'),
         images: ['src/images/**/*.png']
     };
 
+// Process less
+gulp.task('less', ['clean', 'less:main', 'less:wkhtmltopdf']);
+gulp.task('less:main', ['clean'], function () {
+   return gulp.src(paths.styles[0])
+       .pipe(less())
+       .pipe(rename('sbac-ui-kit.min.css'))
+       .pipe(gulp.dest(`${dist}/css`));
+});
+
+gulp.task('less:wkhtmltopdf', ['clean'], function () {
+   return gulp.src(paths.styles[1])
+       .pipe(less())
+       .pipe(rename('sbac-ui-kit-wkhtmltopdf.min.css'))
+       .pipe(gulp.dest(`${dist}/css`));
+});
+
 // Delete the dist directory
 gulp.task('clean', function() {
     return gulp.src(dist)
         .pipe(clean());
-});
-
-// Process less
-gulp.task('less', ['clean'], function () {
-    return gulp.src(paths.styles)
-        .pipe(less())
-        .pipe(rename('sbac-ui-kit.min.css'))
-        .pipe(gulp.dest(`${dist}/css`));
 });
 
 // Fonts
